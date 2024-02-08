5608a196534970ad8cfce21812644bf8e8fd7a0c
Ayo <dannyhopkins20003@gmail.com>
2024-02-08 10:26:58 +0100

    added send page routing

--- app/send/[send]/page.tsx
+++ app/send/[send]/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import gradient from "../assets/gradient.jpg";
-import connect from "../assets/connect.svg";
+import connect from "../../assets/connect.svg";
 import SendMessage from "../../components/SendMessage";
 
 export default function Send({ params }: { params: { send: string } }) {
