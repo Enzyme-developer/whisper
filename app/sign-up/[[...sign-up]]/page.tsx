b92b6416c5ea6c15d31c9712dcf7f8460dbc77cc
Ayo <dannyhopkins20003@gmail.com>
2024-02-08 11:45:06 +0100

    added afterSignupUrl

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="min-h-[100vh] flex items-center justify-center p-12">
-      <SignUp />
+      <SignUp afterSignInUrl="/profile" />
     </div>
   );
 }
