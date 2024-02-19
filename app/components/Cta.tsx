c672ff0af21137dca09285e5cb1dfdc059996138
Ayo <dannyhopkins20003@gmail.com>
2024-02-19 11:54:01 +0100

    added fade In to cta section

--- app/components/Cta.tsx
+++ app/components/Cta.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import FadeInView from "../animations/FadeInView";
 
 const Cta = () => {
   return (
-    <div className="flex flex-col items-center justify-center max-w-5xl my-6 gap-4">
+    <FadeInView className="flex flex-col items-center justify-center max-w-5xl my-6 gap-4">
       <p className="text-[#282828]">
         <strong>Ready to get mysterious?</strong> Click the button below to get
         started!
@@ -11,7 +12,7 @@ const Cta = () => {
       <Button className="bg-orange-500 hover:bg-[#540E38] w-fit" size="lg">
         <Link href="/sign-in">Get Started</Link>
       </Button>
-    </div>
+    </FadeInView>
   );
 };
 
