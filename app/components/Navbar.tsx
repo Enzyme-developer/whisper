0a8f13c28f1a29e14bb3611caddedaf32fbde85a
Ayo <dannyhopkins20003@gmail.com>
2024-03-11 14:11:40 +0100

    removed env and updated navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { useUser } from "@clerk/nextjs";
+import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 import NavActions from "./NavActions";
 import logo from "../assets/logo.png";
 import Image from "next/image";
@@ -16,13 +16,14 @@ const Navbar = () => {
       </Link>
 
       <div className="hidden md:flex items-center space-x-4">
-        {isSignedIn ? (
+        <SignedIn>
           <NavActions />
-        ) : (
+        </SignedIn>
+        <SignedOut>
           <Button className="bg-[#540E38] hover:bg-orange-500">
             <Link href="/messages">Get Started</Link>
           </Button>
-        )}
+        </SignedOut>
       </div>
 
       <Sidebar />
