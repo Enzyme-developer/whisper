551c8b6d559a0f4e4b048d54b319caff2e72ced4
Ayo <dannyhopkins20003@gmail.com>
2024-03-20 22:15:42 +0100

    formatted recipient value to lowercase

--- app/services/createMessage.ts
+++ app/services/createMessage.ts
@@ -5,9 +5,10 @@ export const createNewMessage = async (requestData: {
   recipient: string;
 }) => {
   const { content, recipient } = requestData;
+  const formattedRecipient = recipient.toLowerCase()
   const { data } = await axios.post("/api/message/create-message", {
     content,
-    recipient,
+    formattedRecipient,
   });
 
   if (data.error || !data.createdMessage) {
